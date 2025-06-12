c671334a9ad53fe7ac5a1595ad839f5183714d78
We-Pull <vipuljad3@outlook.com>
2025-06-12 21:15:47 +1000

    fixing script

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-
-chmod 755 main.py
+echo 'creating virtual env venv'
+python3 -m venv venv
 source venv/bin/activate
+echo 'Installing required libraries'
+pip install -r requirements.txt
+chmod 755 main.py
+echo 'Running App'
 export FLASK_APP=main.py
 export FLASK_RUN_PORT=8000
 python3 -m flask run 
